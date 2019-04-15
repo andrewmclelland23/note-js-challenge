805a0a0a1bbac8b3ce6821ff3db6de13c2c75189
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-04-15 16:24:35 +0100

    ADD: Added a NoteListView.prototype.toHTML method to convert notes into an html list

--- spec/noteListViewSpec.js
+++ spec/noteListViewSpec.js
@@ -4,4 +4,15 @@ function noteListViewConstructor() {
   assert.isEqual(noteListView._noteListModel, 1);
 };
 
+function noteListToHTML() {
+  noteListModel = new NoteListModel;
+  noteListModel.addNote('Test 1')
+  noteListModel.addNote('Test 2')
+  noteListModel.addNote('Test 3')
+  noteListView = new NoteListView(noteListModel);
+  console.log('noteListToHTML:');
+  assert.isEqual(noteListView.toHTML(), "<ul><li><div>Test 1</div></li><li><div>Test 2</div></li><li><div>Test 3</div></li></ul>");
+};
+
 noteListViewConstructor();
+noteListToHTML();
--- src/NoteListView.js
+++ src/NoteListView.js
@@ -3,5 +3,14 @@
   function NoteListView(noteListModel) {
     this._noteListModel = noteListModel;
   };
+
+  NoteListView.prototype.toHTML = function() {
+    htmlString = "<ul>"
+    this._noteListModel.noteArray().forEach(function(note) {
+      htmlString += `<li><div>${note.content()}</div></li>`
+    });
+    return htmlString + '</ul>';
+  };
+
   exports.NoteListView = NoteListView;
 })(this);
